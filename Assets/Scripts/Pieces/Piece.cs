34771f03e7f4a2ded47a4b53e4bc8cd93f2d58a5
Karl Bennett <karlthomasbennett@gmail.com>
2021-09-26 12:33:01 +1300

    Add empty piece scripts

---
+++ Assets/Scripts/Pieces/Piece.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Piece : MonoBehaviour
+{
+    public Tile tile;
+    public string team;
+
+    public abstract List<Tile> GetMoves();
+
+    public void HighlightPossibleMoves()
+    {
+        List<Tile> tiles = GetMoves();
+        foreach(Tile t in tiles){
+            t.GetComponent<SpriteRenderer>().color = Color.cyan;
+        }
+    }
+    
+    public void Return()
+    {
+        transform.position = tile.transform.position;
+    }
+
+}
