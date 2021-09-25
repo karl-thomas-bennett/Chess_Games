987207a94a1be81cdccea07c22f54b725fe7369c
Karl Bennett <karlthomasbennett@gmail.com>
2021-09-25 14:06:35 +1200

    Add board creator

---
+++ Assets/Editor/Board_Creator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Board))]
+public class Board_Creator : Editor
+{
+    Board board;
+
+    private void OnSceneGUI()
+    {
+        board = (Board)target;
+        Handles.color = Color.green;
+        for(int i = 0; i < board.tiles.Count; i++)
+        {
+            if(Handles.Button(board.tiles[i].transform.position, Quaternion.identity, 0.5f, 0.5f, Handles.RectangleHandleCap))
+            {
+                board.SetSelected(i);
+            }
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        Board board = (Board)target;
+        if (GUILayout.Button("Generate Board"))
+        {
+            board.GenerateTiles();
+        }
+    }
+}
---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public List<Tile> tiles;
+    public int selected = 0;
+    public int size;
+    public Tile tile;
+    private Color selectedColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateTiles()
+    {
+        int count = transform.childCount;
+        int j = 0;
+        for (int i = 0; i < count; i++)
+        {
+            if (transform.GetChild(j).name.StartsWith("Tile")){
+                DestroyImmediate(transform.GetChild(j).gameObject);
+            }
+            else
+            {
+                j++;
+            }
+            
+        }
+        tiles.Clear();
+        
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                Tile t = Instantiate(tile, transform.position + new Vector3(x, y, 0), transform.rotation, transform);
+                t.GetComponent<SpriteRenderer>().color = new Color((x + y) % 2, (x + y) % 2, (x + y) % 2);
+                tiles.Add(t);
+            }
+        }
+    }
+
+    public void SetSelected(int i)
+    {
+        tiles[selected].GetComponent<SpriteRenderer>().color = selectedColor;
+        selected = i;
+        selectedColor = tiles[selected].GetComponent<SpriteRenderer>().color;
+        tiles[selected].GetComponent<SpriteRenderer>().color = new Color(1, 1, 0);
+
+    }
+}
