f62ffafb35b7b9528fb84d937327eee728635d54
Karl Bennett <karlthomasbennett@gmail.com>
2021-10-03 13:16:07 +1300

    closes #10 Create abstract class for Piece

---
+++ Assets/Scripts/Pieces/Piece.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Piece : MonoBehaviour
+{
+    public string team;
+    public Tile tile;
+    public int id;
+
+    public abstract List<Move> GetMoves();
+}
