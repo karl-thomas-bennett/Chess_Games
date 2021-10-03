47d3514d11bb0bc0ad3d8ab216720df6403bb781
Karl Bennett <karlthomasbennett@gmail.com>
2021-10-03 14:07:58 +1300

    Add constructor to Move

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -10,6 +10,15 @@ public class Move
     public int toId;
     public bool endsTurn;
 
+    public Move(Tile from, Tile to, bool endsTurn = true)
+    {
+        this.from = from;
+        this.to = to;
+        //fromId = from.piece.id;
+        //toId = to.piece.id
+        this.endsTurn = endsTurn;
+    }
+
     public bool MakeMove()
     {
         return false;
