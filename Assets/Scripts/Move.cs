1a3d1c4ab9f026e6112e33ba3c8fe211158f5fcf
Karl Bennett <karlthomasbennett@gmail.com>
2021-10-03 13:12:21 +1300

    closes #9 Create skeleten class for Move

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move 
+{
+    public Tile from;
+    public Tile to;
+    public int fromId;
+    public int toId;
+    public bool endsTurn;
+
+    public bool MakeMove()
+    {
+        return false;
+    }
+
+    public bool UndoMove()
+    {
+        return false;
+    }
+
+}
